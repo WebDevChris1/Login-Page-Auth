2cbdd7b86a25556d7fc77267516ab879ea310e9f
danger2123 <sellmysolenyc@gmail.com>
2021-07-14 16:31:14 -0400

    added comments & cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Login-Page-Auth
+
+Using my previous Login-Page project I added some JavaScript to verify user's log in info showing my understanding for JS objects, if statements and the click event listener.
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const errorMsg = document.getElementById("error-msg");
+const msg = document.getElementById("msg");
 const email = document.getElementById("email");
 const pass = document.getElementById("pass");
 const submit = document.getElementById("submit");
@@ -10,10 +10,14 @@ let auth = {
   pass: "123",
 };
 
+// when submit button is clicked
 submit.addEventListener("click", function () {
+  // stop page reload
   event.preventDefault();
+  // error if nothing is entered
   if (email.value === "" || pass.value === "") {
-    errorMsg.textContent = "- Please enter your email and password";
+    msg.textContent = "Please enter your email and password";
+    // verify email and pass
   } else if (email.value === auth.email && pass.value === auth.pass) {
     email.remove();
     pass.remove();
@@ -21,8 +25,9 @@ submit.addEventListener("click", function () {
     labelEmail.remove();
     labelPass.remove();
     greeting.textContent = "Welcome";
-    errorMsg.textContent = "Please Wait...";
+    msg.textContent = "Please Wait...";
   } else {
-    errorMsg.textContent = "Incorrect Email/Password";
+    // can't verify email and pass
+    msg.textContent = "Incorrect Email/Password";
   }
 });
